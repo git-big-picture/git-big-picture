c0a1fb5c4d6da87733a53174963532db854fa25b
Sebastian Pipping <sebastian@pipping.org>
2010-11-12 00:13:42 +0100

    Smaller fixes to setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,18 @@
 # Copyright (C) 2010 Julius Plenz <julius@plenz.com>
 # Copyright (C) 2010 Valentin Haenel <valentin.haenel@gmx.de>
 # Licensed under GPL v3 or later
+#
+from distutils.core import setup
+from git_big_picture.main import VERSION
 
-from distutils.core import setup, Extension
-import git_big_picture as gbp
 
 setup(name = 'git-big-picture',
-	version = gbp.VERSION,
-	author = 'Sebastian Pipping, Julius Plenz & Valentin Haenel',
-	author_email = 'FIXME@foo.bar',
+	version = VERSION,
+	author = 'Sebastian Pipping, Julius Plenz, and Valentin Haenel',
 	description = 'Visualize git repositories.',
 	url = 'http://git.goodpoint.de/?p=git-big-picture.git;a=summary',
 	license = 'GPL v3 or later',
 	scripts=['git-big-picture'])
+
 
 # vim: set noexpandtab:
