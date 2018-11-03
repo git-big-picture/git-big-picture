f253794f5b7b554751ef9c43ad85b5698c2154cf
Valentin Haenel <valentin@haenel.co>
2018-11-04 00:38:50 +0100

    fix copyright

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 #
 # Copyright (C) 2010    Sebastian Pipping <sebastian@pipping.org>
 # Copyright (C) 2010    Julius Plenz <julius@plenz.com>
-# Copyright (C) 2010-11 Valentin Haenel <valentin.haenel@gmx.de>
+# Copyright (C) 2010-18 Valentin Haenel <valentin.haenel@gmx.de>
 #
 # git-big-picture is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
