6734f8df4918e90ca7514e1b6de7c3e487ccc41e
Sebastian Pipping <sebastian@pipping.org>
2020-12-29 23:46:27 +0100

    pyupgrade: Apply PEP 3120 (default encoding is UTF-8)
    References:
    - https://github.com/asottile/pyupgrade/blob/master/README.md#-coding--comment
    - https://www.python.org/dev/peps/pep-3120/

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 #
 # This file is part of git-big-picture
 #
--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env nosetests
-# -*- coding: utf-8 -*-
 #
 # This file is part of git-big-picture
 #
