edeb7a5a39dfc20b6bf60dcb810e45e81f36c0f5
Sebastian Pipping <sebastian@pipping.org>
2021-12-17 21:23:50 +0100

    Add official support for Python 3.10

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3 :: Only',
     ],
     data_files=[('share/man/man1', ['git-big-picture.1'])],
